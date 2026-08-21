5020fcdf88b5d4cc5dd7a95873719cc8cd21f201
JenifferCastillo <a20226066@pucp.edu.pe>
2026-08-20 20:17:56 -0500

    cambio

--- lab1-gtics/src/main/java/com/pucp/lab1gtics/Lab1GticsApplication.java
+++ lab1-gtics/src/main/java/com/pucp/lab1gtics/Lab1GticsApplication.java
@@ -9,6 +9,7 @@ public class Lab1GticsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Lab1GticsApplication.class, args);
 		System.out.println("Hola mundo");
+		System.out.println("Hola todos");
 	}
 
 }
